950f17f8d9004310ff5871a9c0dde704c899bdaa
eugenehiggins <genotype@gknow.com>
2016-04-18 16:52:46 -0600

    app is excepting mock data of images. Not displaying them yet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
+
+var galleries = require('../mock/galleries.json');
+
+for (var i = 0; i < galleries.length; i++){
+	//console.log(galleries[i].dir);
+	var gallery = galleries[i];
+	for ( var n = 0; n < gallery.images.length; n++){
+		var mypath = appDir + "/public/img/gallery/" + gallery.dir + "/" + gallery.images[n];
+		//var mypath = appDir + "/public/img/gallery/burgerama-black-lips/IMG_5681.JPG";
+		console.log(mypath);
+		fs.stat(mypath, function(err,stats){
+
+			if (err) {
+				//console.log(err);
+				return;
+			}
+
+			if (stats.isFile()) {
+				console.log('true'); 
+			}
+			
+		})
+		
+	}
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	//console.log(galleryList);
+	res.render('index', {
+		title: 'Howdy', 
+		galleries: galleries 
+	});
 });
 
 module.exports = router;
